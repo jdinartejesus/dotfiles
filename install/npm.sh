6d5c63be2d061ff0f8571836a97fa97b508676ad
Dinarte Jesus <jdinartejesus@gmail.com>
2016-01-03 02:07:47 +0100

    Create scripts to automate installation of Apps and packages

---
+++ install/npm.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Install node packages using NPM.
+
+# Install node using nvm
+brew install nvm
+
+# Install and run latest version of Node.
+nvm install 0.10
+nvm use 0.10
+
+# Make sure we’re using the latest NPM.
+npm install npm -g
+
+# Update all the existing packages
+npm update -g
+
+# Packages to install
+packages = {
+  gulp
+  bower
+  webpack
+  eslint
+  eslint-plugin-react
+}
+
+# Install all packages
+npm install -g "${packages[@]}"
