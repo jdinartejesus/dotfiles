f97e60f9f079c781d4792b347374312d9b40d9bc
Dinarte Jesus <jdinartejesus@gmail.com>
2016-03-06 18:01:14 +0100

    Updated list of node pacakges to install

--- install/npm.sh
+++ install/npm.sh
@@ -3,17 +3,29 @@
 # Install node packages using NPM.
 
 #Install node using nvm
-brew install nvm
+which -s brew
+if [[ $? != 0 ]];
+  then
+    # Install Homebrew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install nvm
+else
+    brew install nvm
+fi
 
 # Install and run latest version of Node.
-nvm install 0.10
-nvm use 0.10
+nvm install 5.0
+nvm use 5.0
 
 # Make sure we’re using the latest NPM.
-npm install npm -g
-
-# Update all the existing packages
-npm update -g
+which -s npm
+if [[ $? != 0 ]];
+  then
+    npm install npm -g
+else
+  # Update all the existing packages
+  npm update -g
+if
 
 # Packages to install
 packages=(
@@ -24,7 +36,11 @@ packages=(
   eslint-plugin-react
   lodash
   coffee-script
+  stylus
+  gulp-cli
+  babel-eslint
+  casperjs
 )
 
 # Install all packages
-npm install -g ${packages[@]}
+npm install -g "${packages[@]}"
