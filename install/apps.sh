897e238113b71456fbcca74d2b33d0dd3c4ade7a
Dinarte Jesus <jdinartejesus@gmail.com>
2016-03-06 18:00:29 +0100

    Updated list of apps to install

--- install/apps.sh
+++ install/apps.sh
@@ -1,2 +1,40 @@
 #!/bin/bash
-#TODO: Add all to use in a new computer
+#TODO Add suport for Linux
+
+# Installing Apps using Homebrew.
+
+#Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure brew is installed and updated.
+which -s brew
+if [[ $? != 0 ]];
+  then
+    # Install Homebrew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  brew update
+fi
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Apps to install
+apps=(
+  google-chrome
+  firefox
+  spectacle
+  evernote
+  iterm2
+  slack
+  gimp
+  cheatsheet
+  flux
+  atom
+)
+
+# Install all apps
+brew cask install "${apps[@]}"
